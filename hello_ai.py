3254ace85a11dd96aeb1a54bf419eb27527670c7
ssandsaii <sand.saii_31@hotmail.com>
2016-01-25 12:34:28 +0700

    jubbb hubbub

---
+++ hello_ai.py
@@ -0,0 +1,12 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 19 2015, 20:38:51) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+x="5622792588"
+>>> print(x)
+5622792588
+>>> y="Saenwilai  Phoun"
+>>> print(y)
+Saenwilai  Phoun
+>>> 
